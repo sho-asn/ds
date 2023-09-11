d82da5dc151f0d0a8857db2962f70dc1ad7bec7e
sho-asn <144698049+sho-asn@users.noreply.github.com>
2023-09-12 00:28:38 +0800

    Update covid_anlysis.R

--- covid_anlysis.R
+++ covid_anlysis.R
@@ -4,19 +4,14 @@ set.seed(32201303)
 cvbase = read.csv("PsyCoronaBaselineExtract.csv")
 cvbase <- cvbase[sample(nrow(cvbase), 40000), ]
 
-#1. 
-#(a) 
 dim(cvbase) # dimension
 str(cvbase) # data types
 summary(cvbase) # summary
 unique(cvbase$coded_country) # unique country
 
-#(b)
 # replace NA in employee status to 0
 cvbase[,21:30] <- replace(cvbase[,21:30], is.na(cvbase[,21:30]), 0) 
 
-#2.
-#(a)
 # filter out Indonesia
 corona_Indonesia <- cvbase[cvbase$coded_country == "Indonesia", c(1:49,51:54)]
 # average of each column
@@ -55,7 +50,6 @@ p <- p + guides(fill=guide_legend(title="Group")) # name the title of legend
 p <- p + scale_fill_manual(values=c("orange", "darkgreen"))
 p
 
-#(b)
 # linear model and summary for the predictions of Indonesia 
 fit_01_Indonesia <- lm(c19ProSo01 ~ . - c19ProSo02 - c19ProSo03 - c19ProSo04, data = corona_Indonesia)
 summary(fit_01_Indonesia)
@@ -66,7 +60,6 @@ summary(fit_03_Indonesia)
 fit_04_Indonesia <- lm(c19ProSo04 ~ . - c19ProSo01 - c19ProSo02 - c19ProSo03, data = corona_Indonesia)
 summary(fit_04_Indonesia)
 
-#(c) 
 # linear model and summary for the predictions of other countries
 fit_01_other <- lm(c19ProSo01 ~ . - c19ProSo02 - c19ProSo03 - c19ProSo04, data = corona_others)
 summary(fit_01_other)
@@ -77,8 +70,6 @@ summary(fit_03_other)
 fit_04_other <- lm(c19ProSo04 ~ . - c19ProSo01 - c19ProSo02 - c19ProSo03, data = corona_others)
 summary(fit_04_other)
 
-#3.
-#(a)
 # for Life expectancy 
 le <- read.csv("data.csv",header = TRUE, skip = 1)
 le <- le[le$Year==2019, c(1,3)]
@@ -115,7 +106,6 @@ datafit <- hclust(dist(data[,2:5]), "ave")
 var_names <- data$country
 plot(datafit, hang = -1, xlab = "Country", main="Hierarchical Clustering", labels = var_names)
 
-#(b)
 # abstracting similar country 
 corona_similar <- cvbase[cvbase$coded_country %in% c("Cambodia", "Philippines", "Guatemala", "Thailand", "Bahrain", "Saudi Arabia")
                         , c(1:49,51:54)]
